45257b8fec11982e9244afa2f1079933e5316757
Emilien Guelin <emilien.guelin@free.fr>
2022-11-09 22:53:14 +0100

    Libft V0.55

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:07:18 by eguelin           #+#    #+#             */
-/*   Updated: 2022/11/09 17:53:37 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 22:49:27 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	pos = 0;
 	nchar = 0;
 	len_needle = ft_strlen(needle);
-	while (pos < len && nchar < len_needle && !(s1[pos] && s2[pos]))
+	while (pos < len && nchar < len_needle && haystack[pos])
 	{
-		if (haystack[pos] == needle[pos])
+		if (haystack[pos] == needle[nchar])
 			nchar++;
 		else if (haystack[pos] == needle[0])
 			nchar++;
 		else
 			nchar = 0;
+		pos++;
 	}
 	if (nchar == len_needle)
-		return (haystack + pos - len_needle);
+		return ((char *)(haystack + pos - len_needle));
 	return (NULL);
 }
