b8c9feae75939dfb6d9c50f394a4656db8dc516b
Emilien Guelin <emilien.guelin@free.fr>
2022-11-23 17:45:37 +0100

    update all

--- ft_printf_V2/ft_print_nbr.c
+++ ft_printf_V2/ft_print_nbr.c
@@ -6,15 +6,15 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 12:03:24 by eguelin           #+#    #+#             */
-/*   Updated: 2022/11/23 17:18:21 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 17:40:32 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_nbr(long n)
+int	ft_print_nbr(int n)
 {
-	unsigned long	un;
+	unsigned int	un;
 	int				count;
 
 	un = n;
--- ft_printf_V2/ft_printf.h
+++ ft_printf_V2/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:29:12 by eguelin           #+#    #+#             */
-/*   Updated: 2022/11/23 17:19:30 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 17:43:05 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_print_nbr(long n);
+int	ft_print_nbr(int n);
 int	ft_print_char(char c);
 int	ft_print_str(const char *s);
-int	ft_print_address(unsigned long un);
 int	ft_print_all(char c, va_list arg);
+int	ft_print_address(unsigned long un);
 int	ft_printf(const char *format, ...);
 int	ft_print_base(unsigned long un, unsigned long size_set, char *set);
 
